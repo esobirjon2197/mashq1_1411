2f0984b81c9b04b1291e5cc3be30275c6eae8446
esobirjon2197 <esobirjon2197@gmail.com>
2025-11-14 15:40:01 +0500

    Uncommented code for string manipulation exercises

---
+++ main.py
@@ -0,0 +1,39 @@
+# 1-m
+matn = "web dasturlashi"
+print(matn)
+print(matn.title())
+print(matn.lstrip())
+print(len(matn) % 2)
+
+
+
+# 2-m
+matn = "    MACHINE LEARING    "
+print(matn)
+print(matn.rstrip())
+print(matn.capitalize())
+
+
+# 3-m
+matn = "python java c++"
+print(matn)
+print(matn.upper())
+print(len(matn) % 5)
+
+
+# 4-m
+matn = "   artifial intelligance    "
+print(matn)
+print(matn.strip())
+print(matn.title())
+
+
+# 5-m
+matn = "SQL VA NoSQL"
+print(matn)
+print(matn.lower())
+print(matn.capitalize())
+print(len(matn))
+
+
+
